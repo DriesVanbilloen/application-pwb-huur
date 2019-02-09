d5a7605f2830f5971f30454b904191d81aea7c17
Dries.Vanbilloen <dries.vanbilloen@amplexor.com>
2019-02-09 16:34:22 +0100

    initial commit

---
+++ src/main/java/com/vanbilloen/pbwhuuraanvraag/PwbHuurApplication.java
@@ -0,0 +1,14 @@
+package com.vanbilloen.pbwhuuraanvraag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwbHuurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwbHuurApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/vanbilloen/pbwhuuraanvraag/PwbHuurApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vanbilloen.pbwhuuraanvraag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PwbHuurApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
