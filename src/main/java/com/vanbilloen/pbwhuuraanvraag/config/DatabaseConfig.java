3ef0a408844d8e0dc477f7b20fc22f6be76ae163
Dries.Vanbilloen <dries.vanbilloen@amplexor.com>
2019-02-09 23:59:08 +0100

    Raw data connection needs refinement

--- src/main/java/com/vanbilloen/pbwhuuraanvraag/config/DatabaseConfig.java
+++ src/main/java/com/vanbilloen/pbwhuuraanvraag/config/DatabaseConfig.java
@@ -1,24 +1,22 @@
 package com.vanbilloen.pbwhuuraanvraag.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-
     @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+    public DataSource dataSource() throws SQLException {
+        PGSimpleDataSource basicDataSource = new PGSimpleDataSource();
+        basicDataSource.setUrl("jdbc:postgresql://ec2-46-137-170-51.eu-west-1.compute.amazonaws.com:5432/dctv79frlqboaq");
+        basicDataSource.setUser("hzhepuignvadmn");
+        basicDataSource.setPassword("476d4a99d9e7a0a47c7c4f2f51a9c368ccf417533b19a4424f7bb715d93d43b6");
+        return basicDataSource;
     }
 }
