082d2ce715afb13ba697b9aa00445a86538b6cc8
Dries.Vanbilloen <dries.vanbilloen@amplexor.com>
2019-02-09 22:23:11 +0100

    database integration

--- src/main/java/com/vanbilloen/pbwhuuraanvraag/controllers/ClientDataController.java
+++ src/main/java/com/vanbilloen/pbwhuuraanvraag/controllers/ClientDataController.java
@@ -1,13 +1,15 @@
 package com.vanbilloen.pbwhuuraanvraag.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/client")
 public class ClientDataController {
 
-    @RequestMapping("/")
-    public String index() {
+    @RequestMapping(method = RequestMethod.POST, value = "")
+    public String postUserInformation() {
         return "New controller";
     }
 }
